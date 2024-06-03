a5ea6203bd67c0587803af976701b579879c937e
Jahle <Jahle@ABLACKMAN>
2024-06-03 17:19:07 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Assignment_5._1._2
+{
+    class Program
+    {
+        static int SumOfDigits(int num)
+        {
+            int sum = 0;
+            while (num > 0)
+            {
+                int digit = num % 10;
+                sum += digit;
+                num /= 10;
+            }
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            int result = SumOfDigits(number);
+            Console.WriteLine($"The sum of the digits of the number {number} is: {result}");
+        }
+    }
+}
